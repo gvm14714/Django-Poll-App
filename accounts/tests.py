50d7eae3ae438ff25b2df875a3a644c31ba1a779
gvm <gvm14714@gmail.com>
2024-03-19 22:07:12 -0400

    u

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import MyModel  # Correctly importing MyModel from models.py
 
-# Create your tests here.
+class MyModelTestCase(TestCase):
+    def setUp(self):
+        # Set up data for the whole TestCase
+        MyModel.objects.create(name="Just a test")
+
+    def test_is_named_test(self):
+        """Test that the MyModel object has a name attribute"""
+        test_object = MyModel.objects.get(name="Just a test")
+        self.assertEqual(test_object.name, "Just a test")
+        
+    def test_mymodel_has_name(self):
+        """Test the is_named_test method"""
+        test_object = MyModel.objects.get(name="Just a test")
+        self.assertTrue(test_object.is_named_test())
+
+
+
